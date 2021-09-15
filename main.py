c7d6770fba373f9c835eae3b018e60d417d46e50
After2Dark <43520508+After2Dark@users.noreply.github.com>
2021-09-15 13:30:26 +0300

    new boundary algorithm

--- main.py
+++ main.py
@@ -6,29 +6,13 @@ from numba import njit
 from scipy import ndimage
 
 
-def change_border(image_, dx_0, dx_1, dy_0, dy_1, color):
-    row = np.asarray([color] * image_.shape[0])
-    string = np.asarray([color] * image_.shape[1])
-
-    for _ in range(dx_0):
-        image_ = np.insert(image_, 0, string, 0)
-        row = np.insert(row, 0, color, 0)
-
-    for _ in range(dy_0):
-        image_ = np.insert(image_, 0, row, 1)
-        string = np.insert(string, 0, color, 0)
-
-    for _ in range(dx_1):
-        image_ = np.insert(image_, image_.shape[0], string, 0)
-        row = np.insert(row, 0, color, 0)
-
-    for _ in range(dy_1):
-        image_ = np.insert(image_, image_.shape[1], row, 1)
-    return image_
+# @njit
+def erode(img, kernel, er_img):
+    return img
 
 
 @njit
-def dilate(img, kernel, boun, dil_img):
+def dilate(img, kernel, dil_img, bound):
     img_v = img.shape[0]
     img_h = img. shape[1]
     kl_v = kernel.shape[0]
@@ -37,24 +21,61 @@ def dilate(img, kernel, boun, dil_img):
     kl_cn_h = kl_h // 2
     for i in range(kl_cn_v, img_v - (kl_v - kl_cn_v - 1)):
         for j in range(kl_cn_h, img_h - (kl_h - kl_cn_h - 1)):
-            dil_img[i - kl_cn_v, j - kl_cn_h] = np.max(img[i - kl_cn_v: i + kl_v - kl_cn_v,
-                                                           j - kl_cn_h: j + kl_h - kl_cn_h] * kernel)
+            dil_img[i, j] = np.max(img[i - kl_cn_v: i + kl_v - kl_cn_v, j - kl_cn_h: j + kl_h - kl_cn_h] * kernel)
+
+    # UP BOUNDARY
+    for i in range(kl_cn_v):
+        for j in range(img_h):
+            if kl_cn_h - 1 < j < img_h - (kl_h - kl_cn_h - 1):
+                dil_img[i, j] = max(np.max(img[: i + kl_v - kl_cn_v, j - kl_cn_h: j + kl_h - kl_cn_h] *
+                                           kernel[kl_cn_v - i:, :]), bound)
+            elif kl_cn_h - 1 >= j:
+                dil_img[i, j] = max(np.max(img[: i + kl_v - kl_cn_v, : j + kl_h - kl_cn_h] *
+                                           kernel[kl_cn_v - i:, kl_cn_h - j:]), bound)
+            elif j >= img_h - (kl_h - kl_cn_h - 1):
+                dil_img[i, j] = max(np.max(img[: i + kl_v - kl_cn_v, j - kl_cn_h:] *
+                                           kernel[kl_cn_v - i:, :img_h + kl_cn_h - j]), bound)
+
+    # DOWN BOUNDARY
+    for i in range(img_v - (kl_v - kl_cn_v - 1), img_v):
+        for j in range(img_h):
+            if kl_cn_h - 1 < j < img_h - (kl_h - kl_cn_h - 1):
+                dil_img[i, j] = max(np.max(img[i - kl_cn_v:, j - kl_cn_h: j + kl_h - kl_cn_h] *
+                                           kernel[:kl_cn_v + img_v - i, :]), bound)
+            elif kl_cn_h - 1 >= j:
+                dil_img[i, j] = max(np.max(img[i - kl_cn_v:, :j + kl_h - kl_cn_h] *
+                                           kernel[:kl_cn_v + img_v - i, kl_cn_h - j:]), bound)
+
+            elif j >= img_h - (kl_h - kl_cn_h - 1):
+                dil_img[i, j] = max(np.max(img[i - kl_cn_v:, j - kl_cn_h:] *
+                                           kernel[:kl_cn_v + img_v - i, :img_h + kl_cn_h - j]), bound)
+
+    # LEFT BOUNDARY
+    for i in range(kl_cn_v):
+        for j in range(kl_cn_h, img_v - (kl_v - kl_cn_v - 1)):
+            dil_img[j, i] = max(np.max(img[j - kl_cn_h: j + kl_h - kl_cn_h, :kl_v - kl_cn_v + i] *
+                                       kernel[:, kl_cn_v - i:]), bound)
+
+    # RIGHT BOUNDARY
+    for i in range(img_h - (kl_h - kl_cn_h - 1), img_h):
+        for j in range(kl_cn_h, img_v - (kl_v - kl_cn_v - 1)):
+            dil_img[j, i] = max(np.max(img[j - kl_cn_h: j + kl_h - kl_cn_h, i - kl_cn_h:] *
+                                       kernel[:, :kl_h - kl_cn_h + img_h - i - 1]), bound)
+
     return dil_img
 
 
-# -------------MAIN-------------- #
-image = cv2.imread('image.png', 0)
-core = ndimage.generate_binary_structure(2, 1).astype('int')
-image = change_border(image, core.shape[0] // 2, core.shape[1] // 2,  core.shape[0] - core.shape[0] // 2 - 1,
-                      core.shape[0] - core.shape[1] // 2 - 1, 0)
+if __name__ == '__main__':
+    start_time = time.time()
+    image = cv2.imread('image.png', 0)
+    core = ndimage.generate_binary_structure(2, 1).astype('int')
 
-dilate_image = image.copy()
-start_time = time.time()
-dilate_image = dilate(image, core, 0, dilate_image)
-print(f'{(time.time() - start_time)} seconds')
-dilate_image_png = Image.fromarray(dilate_image)
-dilate_image_png.save("dilate_image.png", "PNG")
+    dilate_image = image.copy()
+    dilate_image = dilate(image, core, dilate_image, 255)
+    Image.fromarray(dilate_image).save("dilate_image.png", "PNG")
 
-# erode_image = erode(image, core, [2, 2], 1, 'white')
-# erode_image_png = Image.fromarray(erode_image)
-# erode_image_png.save("erode_image.png", "PNG")
+    # erode_image = dilate_image.copy()
+    # erode_image = erode(dilate_image, core, erode_image)
+    # Image.fromarray(erode_image).save("erode_image.png", "PNG")
+
+    print(f'{(time.time() - start_time)} seconds')
